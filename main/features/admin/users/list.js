73918945f560a90baf1c472d345dc332c7ed589c
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-07-02 11:13:19 +0200

    Add bower install to npm install.

--- main/features/admin/users/list.js
+++ main/features/admin/users/list.js
@@ -11,17 +11,14 @@ angular.module('bonita.admin.users.list', ['bonita.common.resources']).config(
     [ '$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.when('/admin/users/', '/admin/users');
         $stateProvider
-        .state('adminUserList', {
-            url: '/admin/users',
-            templateUrl: 'features/admin/users/list.html',
-            controller: 'AdminListCtrl'
-        });
+            .state('adminUserList', {
+                url: '/admin/users',
+                templateUrl: 'features/admin/users/list.html',
+                controller: 'AdminListCtrl'
+            });
     }]).controller('AdminListCtrl', [ '$scope', 'User', function ($scope, User) {
-    $scope.searchParams = {
-        p: 0,
-        c: 10
-    };
-    User.search($scope.searchParams).$promise.then(function(users){
-        $scope.users = users.result;
-    });
-}]);
+        $scope.users = User.search({
+            p: 0,
+            c: 10
+        });
+    }]);
