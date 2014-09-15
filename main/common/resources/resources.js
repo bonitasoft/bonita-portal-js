535948f09a6e3a07424c54e50e14fb230a357114
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-09-15 18:56:05 +0200

    Modify search implementation to fix post actions notation.

--- main/common/resources/resources.js
+++ main/common/resources/resources.js
@@ -1,13 +1,13 @@
 (function () {
-    'use strict';
-    /**
-     * @ngdoc service
-     * @name bonita.common.resources.Resources
-     * @description
-     * Define the resources accessible from the Bonita API
-     */
+  'use strict';
+  /**
+   * @ngdoc service
+   * @name bonita.common.resources.Resources
+   * @description
+   * Define the resources accessible from the Bonita API
+   */
 
-    var API_PATH = '../API/';
+  var API_PATH = '../API/';
 
     /**
      * @internal
@@ -42,80 +42,85 @@
         };
     }
 
-    angular.module('org.bonita.common.resources', ['ngResource'])
-        .constant('API_PATH', API_PATH)
+  angular.module('org.bonita.common.resources', ['ngResource'])
+    .constant('API_PATH', API_PATH)
 
-    /**
-     * @ngdoc method
-     * @name Resources#search
-     * @methodOf bonita.common.resources.Resources
-     * @description
-     * the Resources service decorate the $resource to add a new search
-     * function parsing the http header response to find the number of results
-     * for the given resource search
-     */
-        .config(['$provide', function ($provide) {
-            $provide.decorator('$resource', ['$delegate','$http', function ($delegate, $http) {
-                return function (url, paramDefaults, actions, options) {
-                    actions = angular.extend({}, actions, {
-                        'search': {
-                            transformResponse: $http.defaults.transformResponse.concat(searchTransformResponse)
-                        },
-                        'update': {
-                            method: 'PUT'
-                        }
+  /**
+   * @ngdoc method
+   * @name Resources#search
+   * @methodOf bonita.common.resources.Resources
+   * @description
+   * the Resources service decorate the $resource to add a new search
+   * function parsing the http header response to find the number of results
+   * for the given resource search
+   */
+    .config(['$provide', function ($provide) {
+      $provide.decorator('$resource', ['$delegate', '$http', function ($delegate, $http) {
+        return function (url, paramDefaults, actions, options) {
+          actions = angular.extend({}, actions, {
+            'search': {
+              isArray: true,
+              interceptor: {
+                response: function (response) {
+                  response.resource.pagination = parseContentRange(response.headers('Content-Range'));
+                  return response;
+                }
+              }},
+            'update': {
+              method: 'PUT'
+            }
 
-                    });
-                    return $delegate(url, paramDefaults, actions, options);
-                };
-            }]);
-        }])
+          });
+          return $delegate(url, paramDefaults, actions, options);
+        };
+      }]);
+    }])
 
-    /**
-     * @ngdoc service
-     * @name bonita.common.resources:User
-     * @requires $resource
-     * @description
-     *
-     * var user = User.get({ id: 1 });
-     *
-     * User is then empty but can be use in a scope.
-     * It will be filled with its actual values once the http request is back.
-     * We still can use the associated promise to use the data as soon as it gets back.
-     *
-     * user.$promise.then(function (user) {
+  /**
+   * @ngdoc service
+   * @name bonita.common.resources:User
+   * @requires $resource
+   * @description
+   *
+   * var user = User.get({ id: 1 });
+   *
+   * User is then empty but can be use in a scope.
+   * It will be filled with its actual values once the http request is back.
+   * We still can use the associated promise to use the data as soon as it gets back.
+   *
+   * user.$promise.then(function (user) {
              *  console.log(user);
              * });
-     *
-     **/
-        .factory('User', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'identity/user/:id', { id: '@id' });
-        }])
+   *
+   **/
+    .factory('User', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'identity/user/:id', { id: '@id' });
+    }])
 
-        .factory('Case', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'bpm/case/:id', { id: '@id' });
-        }])
+    .factory('Case', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'bpm/case/:id', { id: '@id' });
+    }])
 
-        .factory('Process', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'bpm/process/:id', { id: '@id' });
-        }])
+    .factory('Process', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'bpm/process/:id', { id: '@id' });
+    }])
 
-        .factory('HumanTask', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'bpm/humanTask/:id', { id: '@id' });
-        }])
-        .factory('I18N', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'system/i18ntranslation/');
-        }])
-        .factory('Profile', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'portal/profile/:id', { id: '@id' });
-        }])
-        .factory('Membership', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'identity/membership/:id', { id: '@id' });
-        }])
-        .factory('ProfessionalData', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'identity/professionalcontactdata/:id', { id: '@id' });
-        }])
-        .factory('PersonalData', ['$resource', function ($resource) {
-            return $resource(API_PATH + 'identity/personalcontactdata/:id', { id: '@id' });
-        }]);
+    .factory('HumanTask', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'bpm/humanTask/:id', { id: '@id' });
+    }])
+    .factory('I18N', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'system/i18ntranslation/');
+    }])
+    .factory('Profile', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'portal/profile/:id', { id: '@id' });
+    }])
+    .factory('Membership', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'identity/membership/:id', { id: '@id' });
+    }])
+    .factory('ProfessionalData', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'identity/professionalcontactdata/:id', { id: '@id' });
+    }])
+    .factory('PersonalData', ['$resource', function ($resource) {
+      return $resource(API_PATH + 'identity/personalcontactdata/:id', { id: '@id' });
+    }]);
 })();
