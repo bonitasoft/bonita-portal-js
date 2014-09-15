9d0e24b265e0762b5e5b23d79d1c96e0260bf607
Vincent Elcrin <vincent.elcrin@bonitasoft.com>
2014-09-15 19:05:46 +0200

    Fix test after modifying search behaviour.

--- test/spec/common/resources/resources.spec.js
+++ test/spec/common/resources/resources.spec.js
@@ -1,52 +1,55 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    describe('User', function () {
+  describe('User', function () {
 
-        beforeEach(module('org.bonita.common.resources'));
+    beforeEach(module('org.bonita.common.resources'));
 
-        var $httpBackend, User;
+    var $httpBackend, User;
 
-        beforeEach(inject(function ($q, $rootScope, _$httpBackend_, _User_) {
-            $httpBackend = _$httpBackend_;
-            User = _User_;
-        }));
+    beforeEach(inject(function ($q, $rootScope, _$httpBackend_, _User_) {
+      $httpBackend = _$httpBackend_;
+      User = _User_;
+    }));
 
-        it('should get user specified by the id', inject(function () {
+    it('should get user specified by the id', inject(function () {
 
-            $httpBackend.expectGET('../API/identity/user/123').respond({
-                id: 123
-            });
+      $httpBackend.expectGET('../API/identity/user/123').respond({
+        id: 123
+      });
 
-            var user = User.get({ id: 123 });
-            $httpBackend.flush();
+      var user = User.get({ id: 123 });
+      $httpBackend.flush();
 
-            expect(user.id).toBe(123);
-        }));
+      expect(user.id).toBe(123);
+    }));
 
-        it('should search an users and return an array also containing pagination', inject(function () {
+    it('should search an users and return an array also containing pagination', inject(function () {
 
-            $httpBackend.expectGET('../API/identity/user?c=10&p=0').respond(function () {
-                return [200, [{ id: 1 }, { id: 2 }], {'Content-Range': '0-10/10'}];
-            });
+      $httpBackend.expectGET('../API/identity/user?c=10&p=0').respond(function () {
+        return [200, [
+          { id: 1 },
+          { id: 2 }
+        ], {'Content-Range': '0-10/10'}];
+      });
 
-            var users = User.search({ p: 0, c: 10 });
-            $httpBackend.flush();
+      var users = User.search({ p: 0, c: 10 });
+      $httpBackend.flush();
 
-            expect(users.result).toEqual([{ id: 1 }, { id: 2 }]);
-            expect(users.pagination).toEqual({ total : 10, index : 0, currentPage : 1, numberPerPage : 10 });
-        }));
+      expect(JSON.stringify(users)).toBe('[{"id":1},{"id":2}]');
+      expect(users.pagination).toEqual({ total: 10, index: 0, currentPage: 1, numberPerPage: 10 });
+    }));
 
-        it('should not throw exception when there is no content-range', inject(function () {
+    it('should not throw exception when there is no content-range', inject(function () {
 
-            $httpBackend.expectGET('../API/identity/user?c=10&p=0').respond(function () {
-                return [200, [], {}];
-            });
+      $httpBackend.expectGET('../API/identity/user?c=10&p=0').respond(function () {
+        return [200, [], {}];
+      });
 
-            var users = User.search({ p: 0, c: 10 });
-            $httpBackend.flush();
+      var users = User.search({ p: 0, c: 10 });
+      $httpBackend.flush();
 
-            expect(users.pagination).toEqual({});
-        }));
-    });
+      expect(users.pagination).toEqual({});
+    }));
+  });
 })();
