00bcc10dc260d535a4c3801da23df228009cf199
julien-reboul <julien.reboul@bonitasoft.com>
2014-10-09 18:38:46 +0200

    call REST API and update test

--- main/features/admin/cases/cases-list.js
+++ main/features/admin/cases/cases-list.js
@@ -1,37 +1,25 @@
-(function () {
+(function caseListModuleDefinition() {
     'use strict';
 
-    angular.module('org.bonita.features.admin.cases.list', [])
-        .controller('casesListCtrl',['$scope', '$filter', function($scope, $filter){
-            $scope.content = 'Hello MTF !';
-            $scope.columns = ['ProcessName', 'ProcessVersion', 'CaseId', 'StartDate', 'StartedBy', 'state'];
-            $scope.cases = [
-                {
-                    'ProcessName' : 'Have Fun',
-                    'ProcessVersion' : '1.0',
-                    'CaseId' : 1232455,
-                    'StartDate': $filter('date')(1235465464, 'medium'),
-                    'StartedBy': 'Chuck Norris',
-                    'state': 'Successful'
-                  },
-                  {
-                    'ProcessName' : 'Have Sad',
-                    'ProcessVersion' : '0.2',
-                    'CaseId' : 789,
-                    'StartDate': $filter('date')(12354, 'medium'),
-                    'StartedBy': 'Olivia Newton John',
-                    'state': 'Successful'
-                  },
-                {
-                    'ProcessName' : 'Make Fun',
-                    'ProcessVersion' : '4651.0',
-                    'CaseId' : 0,
-                    'StartDate': $filter('date')(1469873243, 'medium'),
-                    'StartedBy': 'John Travolta',
-                    'state': 'California'
-                  }
-            ]
-            ;
-          }]);
-
+    angular.module('org.bonita.features.admin.cases.list', ['org.bonita.common.resources', 'gettext'])
+    .controller('casesListCtrl',['$scope', 'caseAPI',  function casesListCtrlDefinition($scope, caseAPI){
+        $scope.columns = ['App Name', 'Version', 'Case Id', 'Start Date', 'Started By', 'Current State'];
+        $scope.cases = [];
+        caseAPI.search({
+            p: 0,
+            c: 50,
+            d:['processDefinitionId', 'started_by','startedBySubstitute']
+          }).$promise.then(function mapCases(fullCases){
+            $scope.cases = fullCases && fullCases.data && fullCases.data.map(function selectOnlyInterestingFields (fullCase){
+              var simpleCase = {};
+              simpleCase[$scope.columns[0]] = fullCase && fullCase.processDefinitionId && fullCase.processDefinitionId.name;
+              simpleCase[$scope.columns[1]] = fullCase && fullCase.processDefinitionId && fullCase.processDefinitionId.version;
+              simpleCase[$scope.columns[2]] = fullCase && fullCase.id;
+              simpleCase[$scope.columns[3]] = fullCase && fullCase.start;
+              simpleCase[$scope.columns[4]] = fullCase && fullCase.started_by && ( fullCase.started_by.firstname + ' ' + fullCase.started_by.lastname );
+              simpleCase[$scope.columns[5]] = fullCase && fullCase.state;
+              return simpleCase;
+            });
+          });
+      }]);
   })();
--- test/spec/features/admin/cases/cases-list.spec.js
+++ test/spec/features/admin/cases/cases-list.spec.js
@@ -4,17 +4,258 @@
     describe('admin cases list features', function() {
         it('should fill scope to display case lit', function() {
 
-            var scope;
+            var scope, caseAPI, fullCases;
+
+            fullCases = { data : [
+                {
+                    'id': '1',
+                    'end_date': '',
+                    'startedBySubstitute': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'start': '2014-10-09 16:35:39.270',
+                      'state': 'started',
+                      'rootCaseId': '1',
+                      'started_by': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'processDefinitionId': {
+                        'id': '9208216346116476233',
+                        'icon': '',
+                        'displayDescription': '',
+                        'deploymentDate': '2014-10-09 16:34:46.981',
+                        'description': '',
+                        'activationState': 'ENABLED',
+                        'name': 'Pool',
+                        'deployedBy': '4',
+                        'displayName': 'Pool',
+                        'actorinitiatorid': '1',
+                        'last_update_date': '2014-10-09 16:34:51.047',
+                        'configurationState': 'RESOLVED',
+                        'version': '1.0'
+                      },
+                      'last_update_date': '2014-10-09 16:35:39.270'
+                    },
+                    {
+                      'id': '3',
+                      'end_date': '',
+                      'startedBySubstitute': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'start': '2014-10-09 16:35:51.368',
+                      'state': 'started',
+                      'rootCaseId': '3',
+                      'started_by': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'processDefinitionId': {
+                        'id': '9208216346116476233',
+                        'icon': '',
+                        'displayDescription': '',
+                        'deploymentDate': '2014-10-09 16:34:46.981',
+                        'description': '',
+                        'activationState': 'ENABLED',
+                        'name': 'Pool',
+                        'deployedBy': '4',
+                        'displayName': 'Pool',
+                        'actorinitiatorid': '1',
+                        'last_update_date': '2014-10-09 16:34:51.047',
+                        'configurationState': 'RESOLVED',
+                        'version': '1.0'
+                      },
+                      'last_update_date': '2014-10-09 16:35:51.368'
+                    },
+                    {
+                      'id': '2',
+                      'end_date': '',
+                      'startedBySubstitute': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'start': '2014-10-09 16:35:47.472',
+                      'state': 'started',
+                      'rootCaseId': '2',
+                      'started_by': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'processDefinitionId': {
+                        'id': '9208216346116476233',
+                        'icon': '',
+                        'displayDescription': '',
+                        'deploymentDate': '2014-10-09 16:34:46.981',
+                        'description': '',
+                        'activationState': 'ENABLED',
+                        'name': 'Pool',
+                        'deployedBy': '4',
+                        'displayName': 'Pool',
+                        'actorinitiatorid': '1',
+                        'last_update_date': '2014-10-09 16:34:51.047',
+                        'configurationState': 'RESOLVED',
+                        'version': '1.0'
+                      },
+                      'last_update_date': '2014-10-09 16:35:47.472'
+                    },
+                    {
+                      'id': '4',
+                      'end_date': '',
+                      'startedBySubstitute': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'start': '2014-10-09 16:35:55.251',
+                      'state': 'started',
+                      'rootCaseId': '4',
+                      'started_by': {
+                        'last_connection': '2014-10-09 16:35:30.097',
+                        'created_by_user_id': '-1',
+                        'creation_date': '2014-10-09 14:19:31.623',
+                        'id': '30',
+                        'icon': '/default/icon_user.png',
+                        'enabled': 'true',
+                        'title': 'Mr',
+                        'manager_id': '0',
+                        'job_title': 'Chief Executive Officer',
+                        'userName': 'william.jobs',
+                        'lastname': 'Jobs',
+                        'firstname': 'William',
+                        'password': '',
+                        'last_update_date': '2014-10-09 14:19:31.623'
+                      },
+                      'processDefinitionId': {
+                        'id': '9208216346116476233',
+                        'icon': '',
+                        'displayDescription': '',
+                        'deploymentDate': '2014-10-09 16:34:46.981',
+                        'description': '',
+                        'activationState': 'ENABLED',
+                        'name': 'Pool',
+                        'deployedBy': '4',
+                        'displayName': 'Pool',
+                        'actorinitiatorid': '1',
+                        'last_update_date': '2014-10-09 16:34:51.047',
+                        'configurationState': 'RESOLVED',
+                        'version': '1.0'
+                      },
+                      'last_update_date': '2014-10-09 16:35:55.251'
+                    }
+                  ]};
 
             beforeEach(module('org.bonita.features.admin.cases.list'));
 
             beforeEach(inject(function ($rootScope) {
                 scope = $rootScope.$new();
+                caseAPI = {
+                    search : function(){
+                        return {
+                            '$promise' : {
+                                then : function(method){
+                                    method(fullCases);
+                                  }
+                              }
+                            };
+                      }
+                  };
+
               }));
 
-            it('should fill the scope with "WTF!"', inject(function($controller){
-                $controller('casesListCtrl', {'$scope' : scope});
-                expect(scope.content).toBe('Hello MTF !');
+            it('should fill the scope ', inject(function($controller){
+                $controller('casesListCtrl', {
+                    '$scope' : scope,
+                    'caseAPI' : caseAPI
+                  });
+                expect(scope.columns).toBeDefined();
+                expect(scope.columns.length).toBe(6);
+                expect(scope.cases).toBeDefined();
+                expect(scope.cases.length).toBe(4);
               }));
           });
       });
