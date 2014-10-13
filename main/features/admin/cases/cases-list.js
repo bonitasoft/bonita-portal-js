9882fc657932e8c217954f7a1245ca0f700fe244
julien-reboul <julien.reboul@bonitasoft.com>
2014-10-13 10:39:48 +0200

    add external call on table action and fix protractor tests

--- main/features/admin/cases/cases-list.js
+++ main/features/admin/cases/cases-list.js
@@ -2,24 +2,28 @@
     'use strict';
 
     angular.module('org.bonita.features.admin.cases.list', ['org.bonita.common.resources', 'gettext', 'smart-table'])
-    .controller('casesListCtrl',['$scope', 'caseAPI',  function casesListCtrlDefinition($scope, caseAPI){
+    .controller('casesListCtrl',['$scope', 'caseAPI', function casesListCtrlDefinition($scope, caseAPI){
         $scope.columns = ['AppName', 'Version', 'CaseId', 'StartDate', 'StartedBy', 'CurrentState'];
-        $scope.cases = [];
-        caseAPI.search({
-            p: 0,
-            c: 50,
-            d:['processDefinitionId', 'started_by','startedBySubstitute']
-          }).$promise.then(function mapCases(fullCases){
-            $scope.cases = fullCases && fullCases.data && fullCases.data.map(function selectOnlyInterestingFields (fullCase){
-              var simpleCase = {};
-              simpleCase[$scope.columns[0]] = fullCase && fullCase.processDefinitionId && fullCase.processDefinitionId.name;
-              simpleCase[$scope.columns[1]] = fullCase && fullCase.processDefinitionId && fullCase.processDefinitionId.version;
-              simpleCase[$scope.columns[2]] = fullCase && fullCase.id;
-              simpleCase[$scope.columns[3]] = fullCase && fullCase.start;
-              simpleCase[$scope.columns[4]] = fullCase && fullCase.started_by && ( fullCase.started_by.firstname + ' ' + fullCase.started_by.lastname );
-              simpleCase[$scope.columns[5]] = fullCase && fullCase.state;
-              return simpleCase;
-            });
-          });
+        $scope.searchForCases = function casesSearch(tableState) {
+            console.log(tableState);
+            caseAPI.search({
+                p: 0,
+                c: 50,
+                d: ['processDefinitionId', 'started_by', 'startedBySubstitute']
+            }).$promise.then(function mapCases(fullCases) {
+                    $scope.cases = fullCases && fullCases.data && fullCases.data.map(function selectOnlyInterestingFields(fullCase) {
+                        var simpleCase = {};
+                        simpleCase[$scope.columns[0]] = fullCase && fullCase.processDefinitionId && fullCase.processDefinitionId.name;
+                        simpleCase[$scope.columns[1]] = fullCase && fullCase.processDefinitionId && fullCase.processDefinitionId.version;
+                        simpleCase[$scope.columns[2]] = fullCase && fullCase.id;
+                        simpleCase[$scope.columns[3]] = fullCase && fullCase.start;
+                        simpleCase[$scope.columns[4]] = fullCase && fullCase.started_by && ( fullCase.started_by.firstname + ' ' + fullCase.started_by.lastname );
+                        simpleCase[$scope.columns[5]] = fullCase && fullCase.state;
+                        return simpleCase;
+                    });
+                    //$scope.cases = $filter('orderBy')($scope.cases, tableState.sort.predicate, tableState.sort.reverse);
+               });
+          };
+        $scope.searchForCases();
       }]);
   })();
