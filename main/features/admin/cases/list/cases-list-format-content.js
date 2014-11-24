c03f89bc6cb252d77241af33581e30272958b1dd
dhoko <aurelien.garroux@gmail.com>
2014-11-24 17:49:41 +0100

    Remove binding with the scope for format-content ddirective

--- main/features/admin/cases/list/cases-list-format-content.js
+++ main/features/admin/cases/list/cases-list-format-content.js
@@ -14,46 +14,66 @@
     'gettext',
     'ui.bootstrap',
     'org.bonita.services.topurl'
-  ]).controller('formatContentController', ['$scope', 'manageTopUrl', '$filter',  'gettextCatalog', function ($scope, manageTopUrl, $filter,  gettextCatalog) {
+  ]).factory('contentFactory', function ($filter, manageTopUrl, gettextCatalog) {
 
-    if ($scope.column && $scope.column.date && $scope.caseItem[$scope.column.name] && typeof $scope.caseItem[$scope.column.name] === 'string') {
-      //received date is in a non-standard format...
-      // convert 2014-10-17 16:05:42.626 to ISO-8601 Format 2014-10-17T16:05:42.626Z
-      var dateFormat = gettextCatalog.getString('MM/dd/yyyy h:mm:ss a');
-      $scope.contents = $filter('date')($scope.caseItem[$scope.column.name].replace(/ /, 'T'), dateFormat);
-    } else if ($scope.column && $scope.column.popover) {
-      $scope.contents = '<span class="badge">' + $scope.caseItem[$scope.column.name] + '</span>';
-    } else if ($scope.column && $scope.column.linkToProcess) {
-      var linkToProcess = manageTopUrl.getPath() + manageTopUrl.getSearch() + '#?id=' + $scope.caseItem.processDefinitionId.id + '&_p=processmoredetails'+
-        ((!!Number($scope.processManager))?'pm':'admin') +
-        '&' + manageTopUrl.getCurrentProfile();
-      $scope.contents = '<a id="case-process-link-' + $scope.caseItem.id + '" target="_top" href="' + linkToProcess + '">' + $scope.caseItem[$scope.column.name] + '</a>';
-    } else if ($scope.column && $scope.column.linkToCase) {
-      var linkToCase = manageTopUrl.getPath() + manageTopUrl.getSearch() + '#?id=' + $scope.caseItem.id + '&_p=' + $scope.moreDetailToken + '&' + manageTopUrl.getCurrentProfile();
-      $scope.contents = '<a id="case-detail-link-' + $scope.caseItem.id + '" target="_top" href="' + linkToCase + '">' + $scope.caseItem[$scope.column.name] + '</a>';
-    } else {
-      $scope.contents = $scope.caseItem[$scope.column.name] || gettextCatalog.getString($scope.column.defaultValue);
+    var factory = {};
+
+    function defineContent(column, caseItem, opt) {
+      if (column && column.date && caseItem[column.name] && typeof caseItem[column.name] === 'string') {
+        //received date is in a non-standard format...
+        // convert 2014-10-17 16:05:42.626 to ISO-8601 Format 2014-10-17T16:05:42.626Z
+        var dateFormat = gettextCatalog.getString('MM/dd/yyyy h:mm:ss a');
+        return $filter('date')(caseItem[column.name].replace(/ /, 'T'), dateFormat);
+      } else if (column && column.popover) {
+        return '<span class="badge">' + caseItem[column.name] + '</span>';
+
+      } else if (column && column.linkToProcess) {
+        var linkToProcess = manageTopUrl.getPath() + manageTopUrl.getSearch() + '#?id=' + caseItem.processDefinitionId.id + '&_p=processmoredetails'+
+          ((!!Number(opt.processManager))?'pm':'admin') +
+          '&' + manageTopUrl.getCurrentProfile();
+        return '<a id="case-process-link-' + caseItem.id + '" target="_top" href="' + linkToProcess + '">' + caseItem[column.name] + '</a>';
+
+      } else if (column && column.linkToCase) {
+        var linkToCase = manageTopUrl.getPath() + manageTopUrl.getSearch() + '#?id=' + caseItem.id + '&_p=' + opt.moreDetailToken + '&' + manageTopUrl.getCurrentProfile();
+        return '<a id="case-detail-link-' + caseItem.id + '" target="_top" href="' + linkToCase + '">' + caseItem[column.name] + '</a>';
+
+      } else {
+        return caseItem[column.name] || gettextCatalog.getString(column.defaultValue);
+      }
     }
 
-  }])
-    .directive('formatContent', ['$compile',
-      function ($compile) {
+    factory.load = function load(config, cb) {
+
+      var content;
+      cb = cb || angular.noop;
+      content = defineContent(config.col, config.caseItem, {
+        processManager: config.processManager,
+        moreDetailToken: config.moreDetailToken
+      });
+      cb(content);
+    };
+
+    return factory;
+  })
+    .directive('formatContent', ['$compile', 'contentFactory',
+      function ($compile, contentFactory) {
         return {
           template: '<div></div>',
           replace: true,
           restrict: 'AE',
-          scope: {
-            column: '=',
-            caseItem: '=',
-            moreDetailToken: '@',
-            fillPopover: '&',
-            processManager : '@' //needs the processManager to be '0' or '1' order for controller to process it simply
-          },
-          controller: 'formatContentController',
-          link: function ($scope, $element) {
-            $element.html($scope.contents);
-            //to enable directive injection, we need to compile the created element contents
-            $compile($element)($scope);
+          scope: true,
+          link: function (scope, element, attr) {
+
+            function loadContent(content) {
+              element.html(content);
+            }
+            contentFactory.load({
+              col: JSON.parse(attr.column),
+              caseItem: JSON.parse(attr.caseItem),
+              moreDetailToken: attr.moreDetailToken,
+              processManager: attr.processManager
+            }, loadContent);
+
           }
         };
       }]);
