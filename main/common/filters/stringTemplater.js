8faa9e064c8e6e8f9f8d0b8cb4ed38746cfcf993
julien mege <julien.mege@bonitasoft.com>
2015-02-24 15:33:09 +0100

    Fix jsHint build failure

--- main/common/filters/stringTemplater.js
+++ main/common/filters/stringTemplater.js
@@ -1,14 +1,14 @@
-(function() {
+(function () {
   'use strict';
 
-  angular.module('org.bonitasoft.common.filters.stringTemplater', []).filter('stringTemplater', function() {
-    return function(template, replacement) {
+  angular.module('org.bonitasoft.common.filters.stringTemplater', []).filter('stringTemplater', function () {
+    return function (template, replacement) {
       var templteRE = /\{\}/;
       if (!template || !replacement || typeof template !== 'string') {
         return template;
       }
       if (replacement instanceof Array) {
-        return replacement.reduce(function(newTemplate, currentReplacement) {
+        return replacement.reduce(function (newTemplate, currentReplacement) {
           return newTemplate.replace(templteRE, currentReplacement);
         }, template).replace(/\{\}/g, '');
       } else {
