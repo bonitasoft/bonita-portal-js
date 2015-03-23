8bc915b7623f009ad2348b0cef6707485c041284
Julien Reboul <julien.reboul@bonitasoft.com>
2015-03-23 09:34:41 +0100

    fix jshint error

--- main/common/directives/toggle-button/toggle-button.js
+++ main/common/directives/toggle-button/toggle-button.js
@@ -1,27 +1,31 @@
-(function(){
-    'use strict';
-    angular.module('org.bonitasoft.directives.toggleButton', []).directive('toggleButton', function(){
-        // Runs during compile
-        return {
-            restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-            template: '<input type="checkbox">',
-            replace: true,
-            link: function(scope, iElm, iAttrs) {
-                scope.properties = {enabled : true};
-
-                iElm.prop('checked', !!scope.properties.enabled);
-                iElm.bootstrapToggle({
-                  on: iAttrs.on || 'on',
-                  off: iAttrs.off || 'off'
-                });
-                iElm.change(function(){scope.$apply();});
-                scope.$watch(function(){
-                    return iElm.prop('checked');
-                }, function(){
-                    scope.properties.enabled = !scope.properties.enabled;
-                    console.log(scope.properties.enabled);
-                });
-            }
+(function() {
+  'use strict';
+  angular.module('org.bonitasoft.directives.toggleButton', []).directive('toggleButton', function() {
+    // Runs during compile
+    return {
+      restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+      template: '<input type="checkbox">',
+      replace: true,
+      link: function(scope, iElm, iAttrs) {
+        scope.properties = {
+          enabled: true
         };
-    });
+
+        iElm.prop('checked', !!scope.properties.enabled);
+        iElm.bootstrapToggle({
+          on: iAttrs.on || 'on',
+          off: iAttrs.off || 'off'
+        });
+        iElm.change(function() {
+          scope.$apply();
+        });
+        scope.$watch(function() {
+          return iElm.prop('checked');
+        }, function() {
+          scope.properties.enabled = !scope.properties.enabled;
+          console.log(scope.properties.enabled);
+        });
+      }
+    };
+  });
 })();
