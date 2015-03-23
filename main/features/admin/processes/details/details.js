8dbef2d19936e450cc4ed136c0b3b27d4b86e301
Julien Reboul <julien.reboul@bonitasoft.com>
2015-03-23 02:21:33 +0100

    added menu and toggle button for process more details

---
+++ main/features/admin/processes/details/details.js
@@ -0,0 +1,38 @@
+/* jshint sub:true */
+(function() {
+  'use strict';
+
+  angular.module('org.bonitasoft.features.admin.processes.details', [
+    'ui.router',
+    'ui.bootstrap',
+    'gettext',
+    'org.bonitasoft.services.topurl',
+    'org.bonitasoft.common.directives.bonitaHref',
+    'org.bonitasoft.directives.toggleButton'
+  ])
+    .config(
+      function($stateProvider) {
+        $stateProvider.state('bonita.processesDetails', {
+          url: '/admin/processes/details/:processId',
+          templateUrl: 'features/admin/processes/details/menu.html',
+          //abstract: true,
+          controller: 'processMenuCtrl',
+          controllerAs: 'ctrl'
+        }).state('bonita.processesDetails.information', {
+          url: '/information',
+          /*views: {
+            'case-list': {
+              templateUrl: 'features/admin/cases/list/cases-list.html',
+              controller: 'ActiveCaseListCtrl',
+              controllerAs : 'caseCtrl'
+            }
+          }*/
+        });
+      }
+    )
+    .controller('processMenuCtrl', 
+      function($scope) {
+        $scope.enabled = true;
+      }
+    );
+})();
