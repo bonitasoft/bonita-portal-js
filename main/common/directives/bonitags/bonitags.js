fe329565eaed2562c614d907432f981e57577213
julien-reboul <julien.reboul@bonitasoft.com>
2015-04-01 09:53:19 +0200

    add bootstrap-tags dependency and add bonitags directive

---
+++ main/common/directives/bonitags/bonitags.js
@@ -0,0 +1,31 @@
+(function() {
+	'use strict';
+  /**
+  * org.bonitasoft.common.directies.bonitags Module
+  *
+  * manage tags within a directive
+  */
+  angular.module('org.bonitasoft.common.directives.bonitags', []).directive('bonitags', function($timeout){
+    // Runs during compile
+    return {
+      scope: {
+        tagsSuggestion : '=',
+        tagsSelection : '='
+      }, // {} = isolate, true = child, false/undefined = no change
+      // controller: function($scope, $element, $attrs, $transclude) {},
+      restrict: 'E',
+      template: '<div class="bootstrap-tags"></div>',
+      // templateUrl: '',
+      replace: true,
+      link: function($scope, iElm, iAttrs, controller) {
+          $timeout(function() {
+            $(iElm).tags({
+              readOnly : iAttrs.readOnly ==='' || iAttrs.readOnly === 'true',
+              tagData:['boilerplate', 'tags'],
+              suggestions:['basic', 'suggestions']
+          });
+        }, 0);
+      }
+    };
+  });
+})();
--- main/features/admin/processes/details/information.js
+++ main/features/admin/processes/details/information.js
@@ -1,45 +1,20 @@
-/* globals Bloodhound*/
 (function() {
   'use strict';
 
   angular.module('org.bonitasoft.features.admin.processes.details.information', ['org.bonitasoft.common.filters.date.parser',
-      'jsTag', 'siyfion.sfTypeahead'
+      'org.bonitasoft.common.directives.bonitags'
     ])
-    .controller('processInformationCtrl', function($scope, process, dateParser, JSTagsCollection) {
+    .controller('processInformationCtrl', function($scope, process, dateParser, store, categoryAPI) {
       var vm = this;
       vm.process = process;
       vm.parseAndFormat = dateParser.parseAndFormat;
-      $scope.tags = new JSTagsCollection(['jsTag', 'angularJS']);
-      $scope.jsTagOptions = {
-        'texts': {
-          'inputPlaceHolder': 'Type text here'
-        },
-        'tags': $scope.tags
-      };
-      // Build suggestions array
-      var suggestions = ['jsTag', 'c#', 'java', 'javascript', 'jquery', 'android' , 'php', 'c++', 'python', 'ios', 'mysql', 'iphone', 'sql', 'html', 'css', 'objective-c', 'ruby-on-rails', 'c', 'sql-server', 'ajax', 'xml', '.net', 'ruby', 'regex', 'database', 'vb.net', 'arrays', 'eclipse', 'json', 'django', 'linux', 'xcode', 'windows', 'html5', 'winforms', 'r', 'wcf', 'visual-studio-2010', 'forms', 'performance', 'excel', 'spring', 'node.js', 'git', 'apache', 'entity-framework', 'asp.net', 'web-services', 'linq', 'perl', 'oracle', 'action-script', 'wordpress', 'delphi', 'jquery-ui', 'tsql', 'mongodb', 'neo4j', 'angularJS', 'unit-testing', 'postgresql', 'scala', 'xaml', 'http', 'validation', 'rest', 'bash', 'django', 'silverlight', 'cake-php', 'elgg', 'oracle', 'cocoa', 'swing', 'mocha', 'amazon-web-services'];
-      suggestions = suggestions.map(function(item) { return { "suggestion": item } });
-      
-      // Instantiate the bloodhound suggestion engine
-      suggestions = new Bloodhound({
-        datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.suggestion); },
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        local: suggestions
+      vm.categories = [];
+      vm.selectedCategories = [];
+
+      store.load(categoryAPI, {
+        f: ['id=' + process.id]
+      }).then(function(categories) {
+        [].push.apply(vm.categories, categories);
       });
-
-      // Initialize the bloodhound suggestion engine
-      suggestions.initialize();
-
-      // Single dataset example
-      $scope.exampleData = {
-        displayKey: 'suggestion',
-        source: suggestions.ttAdapter()
-      };
-      
-      // Typeahead options object
-      $scope.exampleOptions = {
-        hint: false,
-        highlight: true
-      };
     });
 })();
