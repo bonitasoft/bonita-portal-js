51bd52f69ee79ad39240091e01e0701851acdec8
julien-reboul <julien.reboul@bonitasoft.com>
2015-05-27 16:51:24 +0200

    add missing task on a wiredep call

--- Gruntfile.js
+++ Gruntfile.js
@@ -484,7 +484,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
-      'wiredep',
+      'wiredep:e2e',
       'injector',
       'lineending',
       'concurrent:server',
