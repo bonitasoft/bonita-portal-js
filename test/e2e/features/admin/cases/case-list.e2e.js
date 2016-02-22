bbd83487ab30e707f58950b5794ba5d077f4381c
Vincent <vincent.elcrin@gmail.com>
2016-02-22 17:03:36 +0100

    Avoid clearing local storage before each test

--- test/e2e/features/admin/cases/case-list.e2e.js
+++ test/e2e/features/admin/cases/case-list.e2e.js
@@ -26,8 +26,6 @@
     beforeEach(function () {
       browser.get('#/admin/cases/list');
       caseList = element(by.css('#case-list'));
-      browser.executeScript('window.sessionStorage.clear();');
-      browser.executeScript('window.localStorage.clear();');
       //browser.debugger(); //launch protractor with debug option and use 'c' in console to continue test execution
     });
 
