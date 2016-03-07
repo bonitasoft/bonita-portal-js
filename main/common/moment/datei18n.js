79c220c28bbc5226fd492657faca390016020905
Colin PUY <colin.puy@bonitasoft.com>
2016-03-07 17:20:58 +0100

    refactor(tasklist): pull up moment service and filter to portal commons
    Pull up moment service + filter from tasklist commons to portal commons
    Use moment service instead of $window.moment

--- main/common/moment/datei18n.js
+++ main/common/moment/datei18n.js
@@ -17,9 +17,12 @@
 (function() {
   'use strict';
 
-  angular.module('org.bonitasoft.common.i18n.filters',['gettext'])
+  angular.module('org.bonitasoft.common.moment')
 
-    .filter('dateI18n', ['$window', 'gettextCatalog', function ($window, gettextCatalog) {
+  /**
+   * @deprecated Use moment filter instead
+   */
+    .filter('dateI18n', function (moment, gettextCatalog) {
       return function translateDate(input, output) {
 
         if(!input) {
@@ -30,29 +33,11 @@
           throw new Error('[com.bonitasoft.common.i18n.filters@dateI18nFilter] You cannot use the date filter without a format');
         }
 
-        if(!$window.moment) {
-          throw new Error('[com.bonitasoft.common.i18n.filters@dateI18nFilter] We need moment.js to translate our dates');
-        }
+        moment.locale(gettextCatalog.currentLanguage || gettextCatalog.baseLanguage);
 
-        $window.moment.locale(gettextCatalog.currentLanguage || gettextCatalog.baseLanguage);
-
-        return $window.moment(+input).format(output);
+        return moment(+input).format(output);
       };
 
-    }])
-    .filter('dateAgo', ['$window', 'gettextCatalog', function ($window, gettextCatalog) {
-      return function dateAgo(input) {
-
-        if(!$window.moment) {
-          throw new Error('[com.bonitasoft.common.i18n.filters@dateAgoFilter] We need moment.js to translate our dates');
-        }
-
-        $window.moment.locale(gettextCatalog.currentLanguage || gettextCatalog.baseLanguage);
-
-        var compar = new Date(+input || Date.now());
-        return $window.moment(compar).fromNow();
-      };
-    }]);
-
+    });
 
 })();
