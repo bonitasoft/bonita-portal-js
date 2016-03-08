f48cbe9055e10cc6c5ab6ffd0e4b3f078d0c9689
Colin PUY <colin.puy@bonitasoft.com>
2016-03-08 14:39:07 +0100

    refactor(tasklist): create task-list module file
    Also rename task-list.js to task-list.routes.js

---
+++ main/features/user/tasks/list/_.module.js
@@ -0,0 +1,17 @@
+(function () {
+
+  'use strict';
+
+  angular.module('org.bonitasoft.features.user.tasks.list', [
+    'org.bonitasoft.features.user.tasks.app',
+    'org.bonitasoft.features.user.tasks.filter',
+    'org.bonitasoft.features.user.tasks.list.table',
+    'org.bonitasoft.features.user.tasks.details',
+    'org.bonitasoft.features.user.tasks.app.config',
+    'org.bonitasoft.templates',
+    'org.bonitasoft.portalTemplates',
+    'common.security',
+    'ui.bootstrap.tpls'
+  ]);
+
+})();
