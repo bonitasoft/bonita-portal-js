1dc8634413955d209accff64337a4876db4d82cc
Colin PUY <colin.puy@bonitasoft.com>
2016-03-29 16:32:36 +0200

    fix(tasklist): enable l10n and xsrf in custom page
    * Split portal js module creation and config
    * Load portal js config in custom page
    
    Closes [BS-15169](https://bonitasoft.atlassian.net/browse/BS-15169)

---
+++ main/app.module.js
@@ -0,0 +1,16 @@
+(function() {
+
+  'use strict';
+
+  angular.module('org.bonitasoft.portal', [
+    'ngCookies',
+    'gettext',
+    'ui.router',
+    'org.bonitasoft.service.features',
+    'org.bonitasoft.common.i18n',
+    'org.bonitasoft.common.resources',
+    'org.bonitasoft.features.admin',
+    'org.bonitasoft.features.user'
+  ]);
+
+})();
