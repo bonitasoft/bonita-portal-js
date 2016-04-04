b17529efceed21fd252f229ce82b6d58b80cb55f
Colin PUY <colin.puy@bonitasoft.com>
2016-04-04 11:11:07 +0200

    test(tasklist): add missing tests for comments.controller.js

--- main/features/user/tasks/list/comments/_.module.js
+++ main/features/user/tasks/list/comments/_.module.js
@@ -3,7 +3,9 @@
   'use strict';
 
   angular.module('org.bonitasoft.features.user.tasks.list.comments', [
-    'org.bonitasoft.common.resources'
+    'org.bonitasoft.common.resources',
+    'org.bonitasoft.features.user.tasks.app.store',
+    'ui.router'
   ]);
 
 })();
