88e387631ba3ea30353a5f5044eef1eec9e227f8
Colin PUY <colin.puy@bonitasoft.com>
2016-04-14 11:03:13 +0200

    feat(tasklist): handle comment adding errors
    Add frontend check for comment size and emptyness
    Add toast on backend error
    Add form is now disabled and we display a message to explain why it is disabled
    
    Closes [BS-15220](https://bonitasoft.atlassian.net/browse/BS-15220)

--- main/features/user/tasks/list/comments/_.module.js
+++ main/features/user/tasks/list/comments/_.module.js
@@ -4,8 +4,9 @@
 
   angular.module('org.bonitasoft.features.user.tasks.list.comments', [
     'org.bonitasoft.common.resources',
+    'org.bonitasoft.common.moment',
     'org.bonitasoft.features.user.tasks.app.store',
-    'ui.router'
+    'ngToast'
   ]);
 
 })();
