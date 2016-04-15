253a9701c8df03c351386350a52646191c5e3c16
Colin PUY <colin.puy@bonitasoft.com>
2016-04-15 16:39:39 +0200

    style(tasklist): style comment section
    Add avatar icons
    Change comment header and content
    Refactor classnames and tags

--- test/e2e/features/user/tasks/comments.e2e.js
+++ test/e2e/features/user/tasks/comments.e2e.js
@@ -16,20 +16,20 @@ import TaskList from './tasklist.page.js';
       var tab = element(by.css('li[heading=Comments] a'));
       tab.click();
 
-      var comments = element.all(by.css('.CaseComments-Comment'));
+      var comments = element.all(by.css('.Comment'));
       expect(comments.count()).toBe(2);
-      expect(comments.get(0).element(by.css('.CaseComments-CommentHeader')).getText()).toBe('Walter Bates added a comment - Mar 9, 2016 4:33 PM');
-      expect(comments.get(0).element(by.css('.CaseComments-CommentContent ')).getText()).toBe('This is a human comment');
-      expect(comments.get(1).element(by.css('.CaseComments-CommentHeader')).getText()).toBe('Hellen Kelly added a comment - Mar 10, 2016 12:22 PM');
-      expect(comments.get(1).element(by.css('.CaseComments-CommentContent')).getText()).toBe('This is another human comment');
+      expect(comments.get(0).element(by.css('.Comment-header')).getText()).toBe('Walter Bates Mar 9, 2016 4:33 PM');
+      expect(comments.get(0).element(by.css('.Comment-content ')).getText()).toBe('This is a human comment');
+      expect(comments.get(1).element(by.css('.Comment-header')).getText()).toBe('Hellen Kelly Mar 10, 2016 12:22 PM');
+      expect(comments.get(1).element(by.css('.Comment-content')).getText()).toBe('This is another human comment');
     });
 
     it('should allow user to add a comment', function() {
       var tab = element(by.css('li[heading=Comments] a'));
       tab.click();
 
-      element(by.css('.CaseComments-AddComment .form-control')).sendKeys('Here is a new comment');
-      element(by.css('.CaseComments-AddComment button[type=submit]')).click();
+      element(by.css('.CommentForm .form-control')).sendKeys('Here is a new comment');
+      element(by.css('.CommentForm button[type=submit]')).click();
 
       // TODO add expectations, not possible with actual mock server
     });
@@ -44,12 +44,12 @@ import TaskList from './tasklist.page.js';
       var tab = element(by.css('li[heading=Comments] a'));
       tab.click();
 
-      expect(element(by.css('.CaseComments-AddComment textarea')).isEnabled()).toBeFalsy();
-      expect(element(by.css('.CaseComments-AddComment button[type=submit]')).isEnabled()).toBeFalsy();
-      expect(element(by.css('.CaseComments-AddComment .CaseComments-DisabledMsg')).getText()).toEqual('The case is archived. You cannot add comment anymore');
+      expect(element(by.css('.CommentForm button[type=submit]')).isEnabled()).toBeFalsy();
+      expect(element(by.css('.CommentForm .CommentForm-disabledMsg')).isPresent()).toBeTruthy();
+      expect(element(by.css('.CommentForm .CommentForm-disabledMsg')).getText()).toBe('The case is archived. You cannot add comments anymore');
     });
 
-    it('should should display archived human comments on a task of an archived case', function() {
+    it('should display archived human comments on a task of an archived case', function() {
       // filter done tasks
       element.all(by.css('.TaskFilters li a')).get(3).click();
 
@@ -59,12 +59,12 @@ import TaskList from './tasklist.page.js';
       var tab = element(by.css('li[heading=Comments] a'));
       tab.click();
 
-      var comments = element.all(by.css('.CaseComments-Comment'));
+      var comments = element.all(by.css('.Comment'));
       expect(comments.count()).toBe(2);
-      expect(comments.get(0).element(by.css('.CaseComments-CommentHeader')).getText()).toBe('Walter Bates added a comment - Mar 8, 2016 4:26 PM');
-      expect(comments.get(0).element(by.css('.CaseComments-CommentContent ')).getText()).toBe('This is an archived human comment');
-      expect(comments.get(1).element(by.css('.CaseComments-CommentHeader')).getText()).toBe('Hellen Kelly added a comment - Mar 12, 2016 4:26 PM');
-      expect(comments.get(1).element(by.css('.CaseComments-CommentContent')).getText()).toBe('This is another archived human comment');
+      expect(comments.get(0).element(by.css('.Comment-header')).getText()).toBe('Walter Bates Mar 8, 2016 4:26 PM');
+      expect(comments.get(0).element(by.css('.Comment-content ')).getText()).toBe('This is an archived human comment');
+      expect(comments.get(1).element(by.css('.Comment-header')).getText()).toBe('Hellen Kelly Mar 12, 2016 4:26 PM');
+      expect(comments.get(1).element(by.css('.Comment-content')).getText()).toBe('This is another archived human comment');
     });
 
   });
