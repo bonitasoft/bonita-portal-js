4760eb82e3ad4067228422edd16a1646a9f3e3d9
Colin PUY <colin.puy@bonitasoft.com>
2016-04-18 11:22:55 +0200

    feat(tasklist): tasklist layout with scrollbars (#215)
    Set a 100% max height layout with scrollables main and detail panels.
    Task detail modal is also 100% height with scrollbar
    Remove iframe height polling and set iframes to 100% height with overflow
    
    To be done: move main panel scroll from panel to tbody
    
    Relate to [BS-15231](https://bonitasoft.atlassian.net/browse/BS-15231)

--- test/e2e/features/user/tasks/tasklist.page.js
+++ test/e2e/features/user/tasks/tasklist.page.js
@@ -1,13 +1,29 @@
-export default class TaskList {
+class DetailsPanel {
+
+  constructor() {
+    this.element = element(by.css('.TaskDetailsPanel'));
+  }
+
+  collapse() {
+    this.element.element(by.css('.SizeBar-reduce')).click();
+  }
+
+  expand() {
+    this.element.element(by.css('.SizeBar-expand')).click();
+  }
+
+  isCollapsed() {
+    return this.element.getAttribute('class')
+      .then(classes => classes.split(' ').indexOf('TaskDetailsPanel--collapsed') !== -1);
+  }
+}
+
+class TaskList {
 
   static get() {
     browser.get((browser.params.urls || {}).userTaskList || '#/user/tasks/list');
     browser.waitForAngular();
     return new TaskList();
-  }
-
-  collapseDetailPanel() {
-    element(by.css('.TaskDetails .SizeBar-reduce')).click();
   }
 
   tableLines() {
@@ -17,4 +33,10 @@ export default class TaskList {
   selectDoneTasksFilter() {
     element(by.css('.TaskFilters li a#done-tasks')).click();
   }
+
+  detailsPanel() {
+    return new DetailsPanel();
+  }
 }
+
+export default TaskList;
