7f80dc49e965f50fe5cce72511baa47a15faddc8
abirembaut <anthony.birembaut@gmail.com>
2018-01-17 14:40:49 +0100

    feat(AC): improve validation messages (#442)
    Covers [BS-17772](https://bonitasoft.atlassian.net/browse/BS-17772)

--- main/features/admin/processes/details/actors-mapping.js
+++ main/features/admin/processes/details/actors-mapping.js
@@ -1,5 +1,5 @@
 /** Copyright (C) 2015 Bonitasoft S.A.
- * BonitaSoft, 31 rue Gustave Eiffel - 38000 Grenoble
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
