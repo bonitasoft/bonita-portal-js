f6ab7713f2814a55b230e2e425511356110fbfd2
julienmege <julien.mege@bonitasoft.com>
2018-01-24 17:23:21 +0100

    Feat/bs 17871 add new bdm page (#447)
    * feat(AC): implement Install BDM page
    
    Covers [BS-17872](https://bonitasoft.atlassian.net/browse/BS-17872)

---
+++ main/features/admin/bdm/add-bdm-popup.controller.js
@@ -0,0 +1,42 @@
+/** Copyright (C) 2017 Bonitasoft S.A.
+ + * Bonitasoft is a trademark of Bonitasoft SA.
+ + * This software file is BONITASOFT CONFIDENTIAL. Not For Distribution.
+ + * For commercial licensing information, contact:
+ + * Bonitasoft, 32 rue Gustave Eiffel – 38000 Grenoble
+ + * or Bonitasoft US, 51 Federal Street, Suite 305, San Francisco, CA 94107
+ + */
+
+(function () {
+  'use strict';
+
+  angular.module('org.bonitasoft.features.admin.bdm')
+    .controller('AddBDMPopupCtrl', AddBDMPopupCtrl);
+
+  function AddBDMPopupCtrl($scope, $modalInstance, FileUploader, gettext) {
+
+    var self = this;
+
+    self.errorUpload = function errorUpload() {
+      $scope.isTechnicalError = true;
+      $scope.error = gettext('BDM has not been uploaded. System was not able to upload the provided BDM file for technical reasons. You can try again later.');
+    };
+
+    self.successUpload = function successUpload(fileItem, response) {
+      fileItem = fileItem || {file: {}};
+      $scope.isUploadSuccess = true;
+      $scope.fileName = fileItem.file.name;
+      $scope.filePath = response || '';
+    };
+
+    $scope.uploader = new FileUploader({
+      autoUpload: true,
+      url: '../portal/fileUpload',
+      onSuccessItem: self.successUpload,
+      onErrorItem: self.errorUpload
+    });
+
+    self.closeModalSuccess = function closeModalSuccess() {
+      $modalInstance.close($scope.filePath);
+    };
+  }
+})();
