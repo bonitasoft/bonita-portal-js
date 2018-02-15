d6f5a6d5fdc706b70bab8f0f10f1bf19a8130051
julienmege <julien.mege@bonitasoft.com>
2018-02-15 13:54:00 +0100

    feat(test) add protractor on community bdm page (#458)
    feat(test) Add protractor on community bdm page
    
    -  Add new mock system in community
    - Use http-mock for test in community
    - Add new mock system in subscription
    - Add new test e2e in subscription to verify ac section
    
    closes [BS-17997](https://bonitasoft.atlassian.net/browse/BS-17997)

---
+++ test/e2e/features/admin/bdm/bdm-view-for-technical-user.e2e.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  var mock = require('protractor-http-mock');
+
+  describe('bdm view for a technical user', () => {
+    afterEach(function(){
+      mock.teardown();
+    });
+
+    it('should display bdm detail information when tenant is paused ', () => {
+      // Load mock defined in e2e/mocks folder
+      mock(['technical-user-session','paused-tenant','uninstalled-bdm']);
+      browser.get('#/admin/bdm');
+
+      expect(element(by.id('displayState')).getText()).toBe('Not installed');
+      expect(element(by.id('lastUpdate')).getText()).toBe('-');
+      expect(element(by.id('updatedBy')).getText()).toBe('-');
+
+      //install
+      let installButton = element(by.tagName('button'));
+      installButton.click();
+      let installPopUpButton = element(by.id('btn-open-modal'));
+      expect(installPopUpButton.getAttribute('disabled')).toEqual('true');
+      let cancelButton = element(by.cssContainingText('.btn', 'Cancel'));
+      cancelButton.click();
+      installButton.click();
+      element(by.css('input[type="file"]')).sendKeys(__filename);
+      installPopUpButton.click();
+
+      element.all(by.css('.modal-dialog')).then(function(items) {
+          expect(items.length).toBe(0);
+      });
+    });
+  });
+
+})();
