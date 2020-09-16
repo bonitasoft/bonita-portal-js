0ef75b565686c436d0142f581b97f54d313aefe8
abirembaut <anthony.birembaut@bonitasoft.com>
2020-09-16 16:19:57 +0200

    fix(e2e) attempt to fix unstable e2e tests on CI (#547)
    - restart browser to fix browser randomly not able to get page right after local storage cleanup
    - separate e2e tests
    - add retart before storage and localization test + sleep
    - move localization test so that it is not executed in custom page tests where it cannot run while the page is loalized correctly

--- test/e2e/features/user/tasks/tasklist.e2e.js
+++ test/e2e/features/user/tasks/tasklist.e2e.js
@@ -14,7 +14,7 @@ import TaskList from './tasklist.page.js';
 
       afterEach(function() {
           browser.executeScript('window.localStorage.clear();');
-          browser.manage().deleteAllCookies();
+          browser.restart();
       });
 
       it('should load a list of task', function() {
@@ -27,15 +27,6 @@ import TaskList from './tasklist.page.js';
           expect(task).toBeDefined();
       });
 
-      it('should search an element', function() {
-          element(by.id('search')).sendKeys('app');
-          element(by.id('search')).sendKeys(protractor.Key.ENTER);
-
-          var tasks = tasklist.getTasks();
-          expect(tasks.count()).toBe(2);
-
-      });
-
       it('should change pagination', function() {
           element(by.css('.bo-Settings')).click();
           var test = element.all(by.css('.bo-TableSettings-content .btn-group button')).first().getText();
@@ -46,75 +37,6 @@ import TaskList from './tasklist.page.js';
               .then(_tasks => tasks = _tasks)
               .then(() => test)
               .then(val => expect(tasks.length).toEqual(parseInt(val, 10)));
-      });
-
-      it('should go to filtered task by case id', function() {
-          tasklist = TaskList.get({ case: 6 });
-          var tasks = tasklist.getTasks();
-          expect(tasks.count()).toBe(2);
-      });
-
-      it('should filter task by case id', function() {
-          element(by.css('#case')).sendKeys('6').sendKeys(protractor.Key.ENTER);
-
-          var tasks = tasklist.getTasks();
-          expect(tasks.count()).toBe(2);
-      });
-
-      it('should filter task by process', function() {
-          element(by.css('.Filter-process .btn')).click();
-
-          var processes = element.all(by.repeater('p in app.processes'));
-          // 2 process + All
-          expect(processes.count()).toEqual(3);
-
-          //select last process
-          processes.last().element(by.css('.processOptionLink')).click();
-          var tasks = tasklist.getTasks();
-          expect(tasks.count()).toBe(2);
-      });
-
-      it('should have a selected list item', function() {
-          var activeFilter = element(by.css('.TaskFilters .active'));
-          expect(activeFilter.isPresent()).toBe(true);
-
-      });
-
-      it('should filter my tasks', function() {
-          // select My task
-          element.all(by.css('.TaskFilters li a')).get(1).click();
-
-          var link = element(by.css('.TaskFilters .active'))
-              .getWebElement()
-              .getText();
-
-          expect(link).toMatch(/my tasks/i);
-
-          var tasks = tasklist.getTasks();
-          expect(tasks.count()).toBe(2);
-      });
-
-      it('should set filter to done', function() {
-          tasklist = TaskList.get({filter: 'done'});
-          var link = element(by.css('.TaskFilters .active'))
-              .getWebElement()
-              .getText();
-          expect(link).toMatch(/done/i);
-
-          var tasks = tasklist.getTasks();
-          expect(tasks.count()).toBe(5);
-      });
-
-      it('should filter done tasks', function() {
-          element.all(by.css('.TaskFilters li a')).get(2).click();
-
-          var link = element(by.css('.TaskFilters .active'))
-              .getWebElement()
-              .getText();
-          expect(link).toMatch(/done/i);
-
-          var tasks = tasklist.getTasks();
-          expect(tasks.count()).toBe(5);
       });
 
       it('should have sorted tasks by default', function() {
@@ -153,40 +75,6 @@ import TaskList from './tasklist.page.js';
           expect(tasks.count()).toBe(1);
       });
 
-
-      it('should toggle tasks filters menu when click toggle filters', function() {
-          var toggleMenu = element(by.css('.FilterToggle'));
-          var taskList = element(by.css('.TaskList'));
-          var originalWidth = 0;
-
-          taskList.getSize()
-              .then(function(size) {
-                  originalWidth = size.width;
-                  toggleMenu.click();
-                  return taskList.getSize();
-              })
-              .then(function(size) {
-                  expect(size.width).toBeGreaterThan(originalWidth);
-                  toggleMenu.click();
-                  return taskList.getSize();
-              })
-              .then(function(size) {
-                  expect(size.width).toBe(originalWidth);
-              });
-      });
-
-    it('should save display mode in local storage', function() {
-      expect(tasklist.detailsPanel().isCollapsed()).toBeFalsy();
-      expect(element(by.css('.TaskFilters')).isDisplayed()).toBeTruthy();          // filter panel
-
-      tasklist.detailsPanel().collapse();
-      element(by.css('.FilterToggle')).click();
-      browser.refresh();
-
-      expect(tasklist.detailsPanel().isCollapsed()).toBeTruthy();
-      expect(element(by.css('.TaskFilters')).isDisplayed()).toBeFalsy();
-    });
-
       it('should toggle Details view menu when click expand', function() {
           var taskList = element(by.css('.TaskList')), originalWidth = 0;
 
@@ -206,21 +94,11 @@ import TaskList from './tasklist.page.js';
               });
       });
 
-
       it('selectAll should select all task', function() {
           element.all(by.css('th .Cell-ckeckbox input[type=checkbox]')).click();
 
           var cb = element.all(by.css('.Line input[type=checkbox]'));
           expect(cb.count()).toBe(26);
-      });
-
-      it('should be localized', function() {
-        browser.manage().addCookie({name:'BOS_Locale', value:'fr'});
-        browser.refresh();
-
-        expect(element(by.css('.TaskFilters #todo-tasks')).getText()).toEqual('A faire');
-        expect(element(by.css('#form-tab')).getText()).toEqual('Formulaire');
-        expect(element(by.css('#btn-group-assigntome')).getText()).toMatch(/Prendre/i);
       });
   });
 })();
