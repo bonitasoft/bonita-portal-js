d50bfc83f7125da53b4de5c5d9e9a39cba755475
Dumitru Corini <dumitru.corini@bonitasoft.com>
2021-06-16 09:44:39 +0200

    feat(bdm install): Add confirmation checkbox to bdm install (#570)
    When the BDM is updated (installed with an existing BDM) a warning is displayed with a checkbox that must be ticked in order to be able to click on the install button
    
    covers [RUNTIME-290](https://bonitasoft.atlassian.net/browse/RUNTIME-290)

--- test/e2e/features/admin/bdm/bdm-view-for-technical-user.e2e.js
+++ test/e2e/features/admin/bdm/bdm-view-for-technical-user.e2e.js
@@ -8,7 +8,7 @@
       mock.teardown();
     });
 
-    it('should display bdm detail information when tenant is paused ', () => {
+    it('should install bdm', () => {
       // Load mock defined in e2e/mocks folder
       mock(['technical-user-session','paused-tenant','uninstalled-bdm']);
       browser.get('#/admin/bdm');
@@ -21,13 +21,48 @@
       let installButton = element(by.tagName('button'));
       installButton.click();
       let installPopUpButton = element(by.id('btn-open-modal'));
-      expect(installPopUpButton.getAttribute('disabled')).toEqual('true');
       let cancelButton = element(by.cssContainingText('.btn', 'Cancel'));
       cancelButton.click();
       installButton.click();
+      expect(installPopUpButton.getAttribute('disabled')).toEqual('true');
       element(by.css('input[type="file"]')).sendKeys(__filename);
+      expect(installPopUpButton.getAttribute('disabled')).toBeNull();
       installPopUpButton.click();
+      setTimeout(() => {
+        element.all(by.css('.modal-dialog')).then(function (items) {
+          expect(items.length).toBe(0);
+        });
+      }, 200);
+    });
 
+    it('should update bdm', () => {
+      // Load mock defined in e2e/mocks folder
+      mock(['technical-user-session','paused-tenant','installed-bdm']);
+      browser.get('#/admin/bdm');
+
+      expect(element(by.id('displayState')).getText()).toBe('Installed');
+      expect(element(by.id('lastUpdate')).getText()).not.toBe('-');
+      expect(element(by.id('updatedBy')).getText()).not.toBe('-');
+
+      //install
+      let installButton = element(by.tagName('button'));
+      installButton.click();
+      let installPopUpButton = element(by.id('btn-open-modal'));
+      let cancelButton = element(by.cssContainingText('.btn', 'Cancel'));
+      let confirmationCheckbox = element(by.css('.modal-footer .callout-warning > input[type=checkbox]'));
+      let confirmationText = element(by.css('.modal-footer .callout-warning > span'));
+      cancelButton.click();
+      installButton.click();
+      expect(installPopUpButton.getAttribute('disabled')).toEqual('true');
+      element(by.css('input[type="file"]')).sendKeys(__filename);
+      expect(installPopUpButton.getAttribute('disabled')).toEqual('true');
+      confirmationCheckbox.click();
+      expect(installPopUpButton.getAttribute('disabled')).toBeNull();
+      confirmationText.click();
+      expect(installPopUpButton.getAttribute('disabled')).toEqual('true');
+      confirmationText.click();
+      expect(installPopUpButton.getAttribute('disabled')).toBeNull();
+      installPopUpButton.click();
       setTimeout(() => {
         element.all(by.css('.modal-dialog')).then(function (items) {
           expect(items.length).toBe(0);
