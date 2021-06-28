81654c8cbae6aeaf95ec75fbe8482f5e023caca8
DumitruCorini <dumitru.corini@gmail.com>
2021-06-28 12:06:17 +0200

    Add e2e tests

--- test/e2e/features/admin/application/details/application-details.e2e.js
+++ test/e2e/features/admin/application/details/application-details.e2e.js
@@ -68,5 +68,51 @@
         expect(element(by.css('#id-not-provided')).isPresent()).toBe(false);
       });
     });
+
+    describe('with non editable application', function() {
+
+      beforeEach(function() {
+        browser.get('#/admin/applications/999');
+      });
+
+      it('should display page and have non editable general information', function() {
+        expect(element(by.css('#id-wrong')).isPresent()).toBe(false);
+        expect(element(by.css('#id-not-provided')).isPresent()).toBe(false);
+        expect(element(by.tagName('back-button')).isDisplayed()).toBe(true);
+        expect(element(by.tagName('h1')).getText()).toEqual('My non editable app (999.0)');
+        expect(element(by.css('img.bonitasoft-default-logo')).getAttribute('src')).toContain('images/bonitasoftLogo.png');
+        expect(element(by.css('img.bonitasoft-default-logo')).getAttribute('title')).toEqual('Core to Bonita platform');
+        expect(element(by.css('#app-profile-value-visibility')).getText()).toContain('All profiles');
+        expect(element(by.css('#app-profile-value')).isPresent()).toBe(false);
+        expect(element(by.css('#app-edit-btn')).isPresent()).toBe(false);
+      });
+
+      it('should have non editable page list', function() {
+        expect(element(by.css('#page-list-add-button')).isPresent()).toBe(false);
+        expect(element(by.css('.btn-action-sethome')).isPresent()).toBe(true);
+        expect(element(by.css('.btn-action-sethome.clickable')).isPresent()).toBe(false);
+        expect(element(by.css('.btn-action-delete')).isPresent()).toBe(false);
+        expect(element.all(by.css('page-list .page-name')).last().getText()).toContain('home - Application home page');
+      });
+
+      it('should have non editable menu list', function() {
+        expect(element(by.css('#menu-list-add-button')).isPresent()).toBe(false);
+        expect(element(by.css('.btn-action-remove')).isPresent()).toBe(false);
+        expect(element(by.css('.btn-action-edit')).isPresent()).toBe(false);
+        expect(element(by.css('.btn-action-add')).isPresent()).toBe(false);
+        expect(element.all(by.css('.list-group-item.menucontainer-no-submenu-items')).first().getText()).toContain('Cases');
+        expect(element(by.css('.list-group-item.menucontainer-has-submenu-items')).getText()).toContain('All');
+        expect(element(by.css('.menucontainer-submenu-exist action-bar')).getText()).toContain('Cases submenu');
+        // Check the menu is not movable
+        expect(element(by.css('.container-menubuilder[data-drop-enabled="false"][data-drag-enabled="false"]')).isPresent()).toBe(true);
+      });
+
+      it('should have editable look and feel', function() {
+        expect(element(by.css('.AvatarUpload-btn')).isDisplayed()).toBe(true);
+        expect(element(by.css('.AvatarUpload-btn')).isEnabled()).toBe(true);
+        expect(element(by.id('app-layout-value')).isPresent()).toBe(true);
+        expect(element(by.id('app-theme-value')).isPresent()).toBe(true);
+      });
+    });
   });
 })();
