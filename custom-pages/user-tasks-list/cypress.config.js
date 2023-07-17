8493f8d87d4b7aade52d5476f923bf0cd1773384
Dumitru Corini <dumitru.corini@gmail.com>
2023-07-17 09:55:19 +0200

    feat(task list custom page): use cypress instead of protractor for e2e test (#651)

---
+++ custom-pages/user-tasks-list/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:9002',
+    specPattern: 'test/specs/**/*.e2e.js',
+    fixturesFolder: 'test/fixtures',
+    screenshotsFolder: 'target/cypress/screenshots',
+    videosFolder: 'target/cypress/videos',
+    viewportWidth: 1920,
+    viewportHeight: 1080
+  }
+});
