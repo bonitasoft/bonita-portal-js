f84d0362b9d438bfdce40ad1a2fa242adafdea37
Bishal Thapaliya <vishal.thapaliya@gmail.com>
2023-07-18 14:00:09 +0200

    fix (pages): Apply debounce correctly (#650)
    * fix (pages): Apply debounce correctly
    
    Covers [RUNTIME-1713](https://bonitasoft.atlassian.net/browse/RUNTIME-1713)

--- main/features/admin/mapping/actors.js
+++ main/features/admin/mapping/actors.js
@@ -103,8 +103,10 @@
 
     };
 
-   vm.searchWithDebounce = function(mySearch) {
-     debounce(vm.search(mySearch), 300);
+    vm.searchWithDebounce = function(mySearch) {
+       debounce(function () {
+         vm.search(mySearch);
+       }, 200);
     };
 
     vm.search({});
