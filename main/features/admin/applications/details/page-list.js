8303c0fd377b9e00c986a6b3104be79fa83066ae
abirembaut <anthony.birembaut@gmail.com>
2025-02-20 10:45:17 +0100

    chore(pages-list): remove isHidden deprecated filter (#704)

--- main/features/admin/applications/details/page-list.js
+++ main/features/admin/applications/details/page-list.js
@@ -35,7 +35,7 @@
         $scope.i18n = i18nMsg.field;
         $scope.application = application;
 
-        store.load(customPageAPI, {f: ['contentType=page', 'isHidden=false']})
+        store.load(customPageAPI, {f: ['contentType=page']})
           .then(function (pages) {
             $scope.customPages = pages;
           });
